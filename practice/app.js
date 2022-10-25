ffe75ed61778e035de8ef5ab744f9ba174465a12
jimkang83 <jim.kang83@gmail.com>
2022-10-25 01:11:11 -0600

    Updated practice

--- practice/app.js
+++ practice/app.js
@@ -47,13 +47,49 @@
 // }
 // plus(5, 10) 
 
-const player = {
-    name: "jim",
-    sayHello: function(otherPersonsName){
-        console.log("hello " + otherPersonsName + " nice to meet you");
-    },
-};
+// const player = {
+//     name: "jim",
+//     sayHello: function(otherPersonsName){
+//         console.log("hello " + otherPersonsName + " nice to meet you");
+//     },
+// };
 
-console.log(player.name);
-player.sayHello("koo");
-player.sayHello("kang");
+// console.log(player.name);
+// player.sayHello("koo");
+// player.sayHello("kang");
+
+// const calculator = {
+//     plus: function (a, b) {
+//         return a + b;
+//     },
+//     minus: function (a, b) {
+//         return a - b;
+//     },
+//     times: function (a, b) {
+//         return a * b;
+//     },
+//     divide: function (a, b) {
+//         return a / b;
+//     },
+//     power: function (a, b) {
+//         return a ** b;
+//     },
+// }
+
+// const plusResult = calculator.plus(2, 3);
+// console.log(plusResult)
+
+
+const age = 96;
+function calculateKrAge (ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const KrAge = calculateKrAge(age);
+console.log(KrAge);
+
+// const age = parseInt (prompt("How old are you?"));
+// console.log(age);
+
+
+
