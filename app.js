424d5661153b9c31a670dfbdbb4f06b41e5747f4
jimkang83 <jim.kang83@gmail.com>
2022-10-28 14:16:29 -0600

    4.6 Loading Username

--- app.js
+++ app.js
@@ -2,21 +2,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-
-// const HIDDEN_CLASSNAME = "hidden";
-
 // const link = document.querySelector("a"); 자주가는 사이트
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSumit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("usernmae", username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove("hidden");
-
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSumit);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSumit);
+} else {
+    paintGreetings(savedUsername);
+}
