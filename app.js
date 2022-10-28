ed5a6ad6ca1999303ce2c5a2dda0ec0482f54336
jimkang83 <jim.kang83@gmail.com>
2022-10-28 12:51:10 -0600

    4.3 Event part 2

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSumit(event) {
     event.preventDefault();
-    // const username = loginInput.value;
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+   event.preventDefault();
+   console.log(event);
+}
+
 loginForm.addEventListener("submit", onLoginSumit);
+link.addEventListener("click", handleLinkClick);
