5eafdcd96f9fd2a5fbaf7925581ca45d04253a59
jimkang83 <jim.kang83@gmail.com>
2022-10-28 13:51:20 -0600

    4.5 Saving Username

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+// const HIDDEN_CLASSNAME = "hidden";
 
 // const link = document.querySelector("a"); 자주가는 사이트
 
 function onLoginSumit(event) {
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    localStorage.setItem("usernmae", username);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove("hidden");
 
