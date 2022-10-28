aef9d64a1903a20b9e2723f9b6b717ca34b0f1c7
jimkang83 <jim.kang83@gmail.com>
2022-10-28 13:33:53 -0600

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+
+// const link = document.querySelector("a"); 자주가는 사이트
 
 function onLoginSumit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove("hidden");
+
 }
 
-function handleLinkClick(event) {
-   event.preventDefault();
-   console.log(event);
-}
+
 
 loginForm.addEventListener("submit", onLoginSumit);
-link.addEventListener("click", handleLinkClick);
