2827411d9106d6dbc1724efa1728122e2fd6acfe
jimkang83 <jim.kang83@gmail.com>
2022-10-31 16:56:25 -0600

    7.8 Deleting ToDos part3

--- js/todo.js
+++ js/todo.js
@@ -13,7 +13,9 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
+    toDos = toDos.filter(toDo => toDo.id != parseInt(li.id));
     li.remove();
+    saveToDos();
 }
 
 function paintToDo(newTodo) {
