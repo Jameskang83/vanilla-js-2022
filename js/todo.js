b71e97c99d7966fa7660e6c1f81c187a3e96f77c
jimkang83 <jim.kang83@gmail.com>
2022-10-31 15:57:15 -0600

    7.6 Deleting ToDos part1

--- js/todo.js
+++ js/todo.js
@@ -18,8 +18,9 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "×";
     button.addEventListener("click", deleteToDo);
@@ -33,14 +34,14 @@ function handleToDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo); 
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj); 
     saveToDos();
 }
-
-// function sayHello(item) {
-//     console.log("this is the turn of", item);
-// }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
@@ -50,5 +51,6 @@ console.log(savedToDos);
 if (savedToDos != null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    parsedToDos.forEach((item) => console.log("this is turn of", item));
+    parsedToDos.forEach(paintToDo);
 }
+
