13fcff198077db5aff6e2d9c6926dacc1c4f4c13
jimkang83 <jim.kang83@gmail.com>
2022-11-01 13:06:40 -0600

    8.2 Main Screen

--- js/background.js
+++ js/background.js
@@ -1,11 +1,21 @@
+
+const mainBgImg = document.getElementById("bg-img");
+
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const img = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+img.src = `img/${chosenImage}`;
+mainBgImg.appendChild(img);
 
-document.body.appendChild(bgImage);
+// const bgImage = document.createElement("img");
 
-
+// const fragment  = document.createDocumentFragment();
+// const img = fragment
+//     .appendChild(document.createElement("section"))
+//     .appendChild(document.createElement("div"))
+//     .appendChild(document.createElement("img"));
+// document.body.appendChild(fragment);
+// document.body.appendChild(img);
